eb126231653b7c12fb3a8a3342b1324588443a29
Younkeryjk <Younkeryjk@163.com>
2019-06-05 16:46:35 +0800

    Add files via upload

---
+++ Java学习笔记/注解/注解方式DBUtil/util/DBUtil.java
@@ -0,0 +1,36 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+   
+/**
+ * @author Younker
+ *
+ * 2019年6月3日
+ * 
+ * 非注解方式DBUtil
+ */
+public class DBUtil {
+    static String ip = "127.0.0.1";
+    static int port = 3306;
+    static String database = "test";
+    static String encoding = "UTF-8";
+    static String loginName = "root";
+    static String password = "admin";
+    static{
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+   
+    public static Connection getConnection() throws SQLException {
+        String url = String.format("jdbc:mysql://%s:%d/%s?characterEncoding=%s", ip, port, database, encoding);
+        return DriverManager.getConnection(url, loginName, password);
+    }
+    public static void main(String[] args) throws SQLException {
+        System.out.println(getConnection());
+    }
+}
