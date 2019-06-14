04312e4ad8c0d4f727b1796709a0850c576473aa
Younkeryjk <Younkeryjk@163.com>
2019-06-14 16:41:11 +0800

    Add files via upload

---
+++ Java学习笔记/j2ee/src/filter/EncodingFilter.java
@@ -0,0 +1,37 @@
+package filter;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EncodingFilter implements Filter {
+
+	@Override
+	public void destroy() {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+		request.setCharacterEncoding("UTF-8");//获取中文参数
+		response.setContentType("text/html;charset=UTF-8");//返回中文的响应
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+	}
+
+}
